02a0ad8bdfd1a1a3b0bb6cccf7d6a2e572a9077c
Nick-tikhe <163338725+Nick-tikhe@users.noreply.github.com>
2025-08-20 10:00:05 +0530

    2nd commit with forcefully fail

--- src/main/java/com/example/demo/JenkinsPracticeApplication.java
+++ src/main/java/com/example/demo/JenkinsPracticeApplication.java
@@ -1,19 +1,22 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsPracticeApplication {
-
+public static final Logger logger=LoggerFactory.getLogger(JenkinsPracticeApplication.class);
 	
-	public static String helloWorldProgram() {
+	public static  helloWorldProgram() {
+		logger.info("1st Hello world program and testing in a logger");
 		return "Hello World";
 	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsPracticeApplication.class, args);
-		
+		logger.info("Application Started 1");
 		System.out.println(helloWorldProgram());
 	}
 
